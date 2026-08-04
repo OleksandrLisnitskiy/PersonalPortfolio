093c6e799e8a997f5c60ebb3f678e11a43f582d9
OleksandrLisnitskiy <88344503+OleksandrLisnitskiy@users.noreply.github.com>
2026-08-04 01:05:17 +0000

    Deploying to gh-pages from @ OleksandrLisnitskiy/PersonalPortfolio@a17d0f4f57e9639ebba9db809c8c0406d19426ed 🚀

---
+++ _app/immutable/chunks/paths.39db3425.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_a2gi55)==null?void 0:s.base)??"/PersonalPortfolio";var a;const t=((a=globalThis.__sveltekit_a2gi55)==null?void 0:a.assets)??e;export{t as a,e as b};
